b47c6260b75e8f40d1531bcb7924225e6edfbf77
loren pabst <lorenpabst@gmail.com>
2017-09-04 14:01:00 -0600

    built function to pull out menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,22 +4,47 @@ import './Header.css';
 
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showMenu: false
+    }
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(){
+    this.setState({
+      showMenu: !this.state.showMenu
+    })
+  }
 
   render() {
+
+    let menuStyle = null;
+    if (this.state.showMenu){
+      menuStyle = {
+        right: '10px'
+      }
+    }else{
+      menuStyle= {
+        right: '-150px'
+      }
+    }
+
     return (
       <div className="header">
 
-        <Link className="header_logo" to="/">HQ</Link>
+        <Link to='/' className="header_logo" to="/">HQ</Link>
         
         <div className='right_nav'>
-          <div className='ham'>...</div>
-          <ul className='menu'>
-            <li>WORK</li>
-            <li>OUR STORY</li>
-            <li>BLOG</li>
-            <li>CAREERS</li>
-            <li>CONTACT</li>
-          </ul>
+          <div className='ham' onClick={ this.toggleMenu }>...</div>
+          <div className='menu' style={menuStyle}>
+            <Link to='/work'>WORK</Link>     
+            <Link to='/story'>OUR STORY</Link>
+            <Link to='/blog'>BLOG</Link>
+            <Link to='/careers'>CAREERS</Link>
+            <Link to='/contact'>CONTACT</Link>
+          </div>
         </div>
 
       </div>
