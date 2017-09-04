4a5b9a596688f4c47d549e1c3c04eca2cf3ee236
loren pabst <lorenpabst@gmail.com>
2017-09-04 09:26:27 -0600

    added nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './Nav.css';
+
+
+class Nav extends Component {
+
+  render() {
+    return (
+      <div className="nav">
+
+          Navigation Component
+
+      </div>
+    );
+  }
+}
+
+
+export default Nav;
